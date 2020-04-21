066bc28e602b3d4f80f76de1618235316e3a9174
citang <citang.wxr@gmail.com>
2020-04-21 22:58:17 +0800

    init

---
+++ src/main/java/com/citang/onema/OnemaApplication.java
@@ -0,0 +1,13 @@
+package com.citang.onema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/citang/onema/OnemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citang.onema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
